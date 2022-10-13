31c9b536dd2e61b30152b0aa3e02412cc35d96d1
Nemanja Stojanovic <stojanovic.nemanja98@gmail.com>
2022-10-13 23:32:28 +0200

    Forbids visits to profile url if not authenticated

--- src/tests/AuthRoutesTests.java
+++ src/tests/AuthRoutesTests.java
@@ -6,11 +6,21 @@ import org.testng.annotations.Test;
 public class AuthRoutesTests extends BasicTest{
 	
 	@Test (priority = 10)
-	public void  ForbidsVisitsToHomeUrlIfNotAuthenticated() {
+	public void ForbidsVisitsToHomeUrlIfNotAuthenticated() {
 		driver.get(baseUrl + "/home"); 
 		Assert.assertTrue(driver.getCurrentUrl()
 				.contains("/login"),
 				"Url should contain '/login'");
 	}
+	
+	@Test (priority = 20)
+	public void ForbidsVisitsToProfileUrlIfNotAuthenticated() {
+		driver.get(baseUrl + "/profile");
+		Assert.assertTrue(driver.getCurrentUrl()
+				.contains("/login"),
+				"Url should contain '/login'");
+	}
+	
+	
 	
 }
