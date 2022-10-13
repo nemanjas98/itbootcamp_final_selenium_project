cd10e95304ee512480c24d81533b3713a9c4de59
Nemanja Stojanovic <stojanovic.nemanja98@gmail.com>
2022-10-13 23:31:25 +0200

    Forbids visits to home url if not authenticated

---
+++ src/tests/AuthRoutesTests.java
@@ -0,0 +1,16 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AuthRoutesTests extends BasicTest{
+	
+	@Test (priority = 10)
+	public void  ForbidsVisitsToHomeUrlIfNotAuthenticated() {
+		driver.get(baseUrl + "/home"); 
+		Assert.assertTrue(driver.getCurrentUrl()
+				.contains("/login"),
+				"Url should contain '/login'");
+	}
+	
+}
